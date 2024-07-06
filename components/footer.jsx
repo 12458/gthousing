57ab50ed15d279c73c5c0845dab8d774571554cf
Sim Shang En <sim@shangen.org>
2024-07-06 16:23:01 +0800

    Update source code link

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,6 +2,7 @@ const Footer = () => {
     return (
       <footer className="w-full py-4 text-white text-center">
         <p>Made with ❤️ by <a href="https://about.shangen.org">Shang En</a></p>
+        <p><a href="https://github.com/12458/gthousing">Source Code</a></p>
       </footer>
     );
   };
