83584b8823af531e22e79353caf74854d88ee803
Sim Shang En <sim@shangen.org>
2024-07-06 16:28:08 +0800

    Make footer visible in light mode

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="w-full py-4 text-white text-center">
+      <footer className="w-full py-4 text-black dark:text-white text-center">
         <p>Made with ❤️ by <a href="https://about.shangen.org">Shang En</a></p>
         <p><a href="https://github.com/12458/gthousing">Source Code</a></p>
       </footer>
